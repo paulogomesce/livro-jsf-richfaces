b24acce68723f2e55d9b17d8a83eb08e86feda14
paulo.gomes <dbc.pgomes@unimedpoa.com.br>
2021-12-21 01:06:54 -0300

    Criação do managedBean InformacoesBean

---
+++ cap02/JavaSource/br/com/jsf/presentation/to/Cidade.java
@@ -0,0 +1,27 @@
+package br.com.jsf.presentation.to;
+
+import java.io.Serializable;
+
+public class Cidade implements Serializable{
+
+	private static final long serialVersionUID = 461050819069261108L;
+	
+	private String nome;
+	
+	public Cidade(String nome) {
+		this.nome = nome;
+	}	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	
+
+}
