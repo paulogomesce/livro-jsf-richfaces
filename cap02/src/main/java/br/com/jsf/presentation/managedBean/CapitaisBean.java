20fa848ff964db60d19873047e4de02ccf0239f8
paulo.gomes <dbc.pgomes@unimedpoa.com.br>
2022-01-05 01:57:59 -0300

    Início dos exemplos de componentes compostos

---
+++ cap02/src/main/java/br/com/jsf/presentation/managedBean/CapitaisBean.java
@@ -0,0 +1,26 @@
+package br.com.jsf.presentation.managedBean;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class CapitaisBean {
+	
+	private String capital = "Vitória - Espirito Santo";
+	private int zoom = 5;
+	
+	public String getCapital() {
+		return capital;
+	}
+	public void setCapital(String capital) {
+		this.capital = capital;
+	}
+	public int getZoom() {
+		return zoom;
+	}
+	public void setZoom(int zoom) {
+		this.zoom = zoom;
+	}
+	
+	
+
+}
