b3730f0559f934d9f1bb6f534c0dbce130c85664
paulo.gomes <dbc.pgomes@unimedpoa.com.br>
2022-02-26 02:27:12 -0300

    Configuração do Richfaces e implementação de exemplos

---
+++ cap02/src/main/java/br/com/jsf/presentation/managedBean/SkinBean.java
@@ -0,0 +1,52 @@
+package br.com.jsf.presentation.managedBean;
+
+import java.util.Date;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.event.ValueChangeEvent;
+
+@ManagedBean
+@SessionScoped
+public class SkinBean {
+	
+	private String nomeSkin = "deepMarine";
+	private Date dataAtual;
+	private String[] opcoesSkins = {"DEFAULT", "plain", "emeraldTown", "blueSky", "wine", "japanCherry", "ruby", "classic", "deepMarine"};
+	
+	public void processar() {
+		this.dataAtual = new Date();
+	}	
+	
+	public void alteraSkin(ValueChangeEvent event) {
+		this.nomeSkin = (String)event.getNewValue();
+	}
+
+	public String getNomeSkin() {
+		return nomeSkin;
+	}
+
+	public void setNomeSkin(String nomeSkin) {
+		this.nomeSkin = nomeSkin;
+	}
+
+	public String[] getOpcoesSkins() {
+		return opcoesSkins;
+	}
+
+	public void setOpcoesSkins(String[] opcoesSkins) {
+		this.opcoesSkins = opcoesSkins;
+	}
+
+	public Date getDataAtual() {
+		return dataAtual;
+	}
+
+	public void setDataAtual(Date dataAtual) {
+		this.dataAtual = dataAtual;
+	}
+	
+	
+	
+
+}
